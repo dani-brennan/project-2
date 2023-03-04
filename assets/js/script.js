641193579dcde49eebca4764ef971edc0eb39e07
dani-brennan <danielle.brennan95@gmail.com>
2023-03-04 17:18:06 +0000

    Update buildBlocks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,27 @@ class block {
         this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
         this.bottomLeft = [xAxis, yAxis]
         this.bottomRight = [xAxis + blockWidth, yAxis]
-       
-
     }
 
 }
 
+//all blocks 
+const blocks = [
+    new block(10,370)
+]
+
+console.log(blocks[0])
+
 //build blocks
-function buildBlock() {
+function buildBlocks() {
+
+for (let i=0; i < blocks.length; i++) {
 const block = document.createElement('div')
 block.classList.add('block')
+block.style.left = blocks[i].bottomLeft[0] + 'px'
+block.style.bottom = blocks[i].bottomLeft[1] + 'px'
 gamearea.appendChild(block)
+    }
 }
 
-buildBlock()
+buildBlocks()
