5db4a401e0f579b72c6a97fc0b5409772aa0f4e0
dani-brennan <danielle.brennan95@gmail.com>
2023-03-04 16:56:57 +0000

    Add block class constructor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 const gamearea = document.querySelector('.gamearea')
+const blockWidth = 80
+const blockHeight = 20
 
+//create block
+class block {
+    constructor(xAxis, yAxis) {
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+       
+
+    }
+
+}
 
 //build blocks
 function buildBlock() {
