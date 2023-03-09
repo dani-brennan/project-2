df706c70add0c7421128434cc13fc1f819569347
dani-brennan <danielle.brennan95@gmail.com>
2023-03-09 20:37:49 +0000

    Add missing semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,63 @@
-const gamearea = document.querySelector('.gamearea')
-const blockWidth = 50
-const blockHeight = 20
-const paddleStart = [190,10]
-const gameAreaWidth = 500
-const gameAreaHeight = 400
-const paddleWidth = 100
-const paddleHeight = 20
-const ballStart = [230,30]
-const ballDiameter = 15
-const scoreboard = document.querySelector('.scoreboard')
+const gamearea = document.querySelector('.gamearea');
+const blockWidth = 50;
+const blockHeight = 20;
+const paddleStart = [190,10];
+const gameAreaWidth = 500;
+const gameAreaHeight = 400;
+const paddleWidth = 100;
+const paddleHeight = 20;
+const ballStart = [230,30];
+const ballDiameter = 15;
+const scoreboard = document.querySelector('.scoreboard');
 
-let xDirection = 2
-let yDirection = 2
+let xDirection = 2;
+let yDirection = 2;
 
-let currentPosition = paddleStart
-let ballPosition = ballStart
+let currentPosition = paddleStart;
+let ballPosition = ballStart;
 
-let timerId
+let timerId;
 
 let score = 0;
 let lives = 3;
 
 // Create the start game variable.
-let startGame = false
+let startGame = false;
 
 // Make a reset Button
-const resetButton = document.createElement('button')
-resetButton.innerText = "Reset Game"
-document.body.appendChild(resetButton)
+const resetButton = document.createElement('button');
+resetButton.innerText = "Reset Game";
+document.body.appendChild(resetButton);
 
 // Make a start Button
-const startButton = document.createElement('button')
-startButton.innerText = "Start Game"
-document.body.appendChild(startButton)
+const startButton = document.createElement('button');
+startButton.innerText = "Start Game";
+document.body.appendChild(startButton);
 
 
-// Listen for the button to be pressed
+// Listen for the spacebar to be pressed
 startButton.addEventListener('keydown', (e) => {
     if (e.code === "Space") {
-        // Do your thing
-        
-
+        // stop the start button from being click again
         startButton.addEventListener('click', () => {
           startButton.remove();
-        })
+    
+        });
+ 
     // Make the start game variable true
-    startGame = true
+    startGame = true;
     
         // If the start game variable is true and the ball is not at the bottom of the level, start the game
         if (startGame= true && !ballPosition[1] <= 0){
-
         
 
         //create block
         class block {
             constructor(xAxis, yAxis) {
-                this.topLeft = [xAxis, yAxis + blockHeight]
-                this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
-                this.bottomLeft = [xAxis, yAxis]
-                this.bottomRight = [xAxis + blockWidth, yAxis]
+                this.topLeft = [xAxis, yAxis + blockHeight];
+                this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+                this.bottomLeft = [xAxis, yAxis];
+                this.bottomRight = [xAxis + blockWidth, yAxis];
             }
 
         }
@@ -99,35 +98,35 @@ startButton.addEventListener('keydown', (e) => {
             
             //fifth row
             new block(225,275),
-        ]
+        ];
 
-        console.log(blocks[0])
+        console.log(blocks[0]);
 
         //build blocks
         function buildBlocks() {
 
         for (let i=0; i < blocks.length; i++) {
-        const block = document.createElement('div')
-        block.classList.add('block')
-        block.style.left = blocks[i].bottomLeft[0] + 'px'
-        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
-        gamearea.appendChild(block)
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.left = blocks[i].bottomLeft[0] + 'px';
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+        gamearea.appendChild(block);
             }
         }
 
-        buildBlocks()
+        buildBlocks();
 
         //create paddle
 
-        const paddle = document.createElement('div')
-        paddle.classList.add('paddle')
-        gamearea.appendChild(paddle)
-        drawPaddle()
+        const paddle = document.createElement('div');
+        paddle.classList.add('paddle');
+        gamearea.appendChild(paddle);
+        drawPaddle();
 
         //draw paddle
         function drawPaddle () {
-        paddle.style.left = currentPosition[0] + 'px'
-        paddle.style.bottom = currentPosition[1] + 'px'
+        paddle.style.left = currentPosition[0] + 'px';
+        paddle.style.bottom = currentPosition[1] + 'px';
         }
 
         //move paddle
@@ -135,60 +134,60 @@ startButton.addEventListener('keydown', (e) => {
             switch(e.key) {
                 case 'ArrowLeft':
                     if (currentPosition[0] > 0) {
-                    currentPosition[0] -= 10
-                    drawPaddle()
+                    currentPosition[0] -= 10;
+                    drawPaddle();
                 }
 
                 break;
 
                 case 'ArrowRight':
                     if (currentPosition[0] < gameAreaWidth - paddleWidth) {
-                    currentPosition[0] += 10
-                    drawPaddle()
+                    currentPosition[0] += 10;
+                    drawPaddle();
                 }
                 break;
             }
         }
-        document.addEventListener('keydown', movePaddle)
+        document.addEventListener('keydown', movePaddle);
 
         //draw ball
         function drawBall() {
-            ball.style.left = ballPosition[0] + 'px'
-            ball.style.bottom = ballPosition[1] + 'px'}
+            ball.style.left = ballPosition[0] + 'px';
+          ball.style.bottom = ballPosition[1] + 'px'; }
 
         //ball
-        const ball = document.createElement('div')
-        ball.classList.add('ball')
+        const ball = document.createElement('div');
+        ball.classList.add('ball');
         /*drawBall()*/
 
-        gamearea.appendChild(ball)
+        gamearea.appendChild(ball);
 
         //move ball
         function moveBall() {
-            ballPosition[0] += xDirection
-            ballPosition[1] += yDirection
-            drawBall()
-            collision()
+            ballPosition[0] += xDirection;
+            ballPosition[1] += yDirection;
+            drawBall();
+            collision();
 
         }
-        timerId = setInterval(moveBall, 20)
+        timerId = setInterval(moveBall, 20);
 
         function changeDirection() {
             if (xDirection === 2 && yDirection === 2) {
-            yDirection = -2
-            return
+            yDirection = -2;
+            return;
             }
             if (xDirection === 2 && yDirection === -2) {
-            xDirection = -2
-            return
+            xDirection = -2;
+            return;
             }
             if (xDirection === -2 && yDirection === -2) {
-            yDirection = 2
-            return
+            yDirection = 2;
+            return;
             }
             if (xDirection === -2 && yDirection === 2) {
-            xDirection = 2
-            return
+            xDirection = 2;
+            return;
             }
         }
 
@@ -203,16 +202,16 @@ startButton.addEventListener('keydown', (e) => {
                         ballPosition[1] < blocks[i].topLeft[1]) 
                 ) {
                         //remove block class
-                        const allBlocks = Array.from(document.querySelectorAll('.block'))
-                        allBlocks[i].classList.remove('block')
-                        blocks.splice (i, 1)
-                        changeDirection()
-                        score++
-                        scoreboard.innerHTML = score
+                        const allBlocks = Array.from(document.querySelectorAll('.block'));
+                        allBlocks[i].classList.remove('block');
+                        blocks.splice (i, 1);
+                        changeDirection();
+                        score++;
+                        scoreboard.innerHTML = score;
                         if (blocks.length === 0) {
-                            scoreboard.innerHTML = 'You win!'
-                            clearInterval(timerId)
-                            document.removeEventListener('keydown', movePaddle)
+                            scoreboard.innerHTML = 'You win!';
+                            clearInterval(timerId);
+                            document.removeEventListener('keydown', movePaddle);
                         }
                     }
                 
@@ -225,7 +224,7 @@ startButton.addEventListener('keydown', (e) => {
                 ballPosition[1] >= (gameAreaHeight - ballDiameter))
                 
             {
-                changeDirection()
+                changeDirection();
             }
 
             //if the ball hits the paddle
@@ -235,20 +234,20 @@ startButton.addEventListener('keydown', (e) => {
             (ballPosition[1] > currentPosition[1] && 
             ballPosition[1] < currentPosition[1] + paddleHeight))
             {
-                changeDirection()
+                changeDirection();
             }
 
                 //game over
                 if (ballPosition[1] <= 0) {
-                    clearInterval(timerId)
-                    scoreboard.innerHTML = 'Game Over...'
-                    document.removeEventListener('keydown', movePaddle) 
+                    clearInterval(timerId);
+                    scoreboard.innerHTML = 'Game Over...';
+                    document.removeEventListener('keydown', movePaddle);
                     // Change start game to false so it does not redraw over itself
-                    startGame = false
+                    startGame = false;
                     // Add a button to reload the page
                     resetButton.addEventListener('click', () => {
-                        location.reload()
-                    })
+                        location.reload();
+                    });
                 }
 
 
@@ -256,4 +255,4 @@ startButton.addEventListener('keydown', (e) => {
             
             }
         }}
-    })
+    });
